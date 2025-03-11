4bd42b30f948a10c7c6d7cc754419542f60c7c1a
Ankit Verma <ankitverma10203@gmail.com>
2025-03-11 07:57:38 +0530

    remove register form

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import LoginForm from "./component/authentication/LoginForm";
-import RegisterForm from "./component/authentication/RegisterForm";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserDashboard from "./component/dashboard/UserDashboard";
 import ApprovalPage from "./component/approval/ApprovalPage";
@@ -57,7 +56,6 @@ function App() {
                 <Routes>
                   <Route path="/" element={<LoginForm />} />
                   <Route path="/callback" element={<Callback />} />
-                  <Route path="/register" element={<RegisterForm />} />
                   <Route
                     path="/dashboard"
                     element={<AuthenticationGuard component={UserDashboard} />}
